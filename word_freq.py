98727084f3bef301668a0b142dca172eea85c01c
A-Mikhail <mikhail.adamenko@protonmail.com>
2017-08-07 14:50:17 +0600

    Change README.md, added new required argument in word_freq.py instead of hardcoded words argument.

--- word_freq.py
+++ word_freq.py
@@ -8,13 +8,12 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "wordOne",
-        help="the word to be compared with the second."
-    )
-
-    parser.add_argument(
-        "wordTwo",
-        help="the word to be compared with the first."
+        "-l", 
+        "--list",
+        nargs="+",
+        dest="list",
+        required=True,
+        help="the word or list of the words to be compared."
     )
 
     parser.add_argument(
@@ -31,9 +30,9 @@ def main():
         sys.stderr.write("Error: " + args.filename + " does not exist!")
         sys.exit(1)
 
-    word_freq(args.wordOne, args.wordTwo, args.filename)
+    word_freq(args.list, args.filename)
 
-def word_freq(wordOne, wordTwo, filename):
+def word_freq(wordArg, filename):
     doc = {}
 
     for line in open(filename, 'r', encoding="utf-8"):
@@ -45,58 +44,39 @@ def word_freq(wordOne, wordTwo, filename):
             else:
                 doc[entry] = 1
 
-    if (wordOne not in doc):
-        sys.stderr.write("Error: " + wordOne + " does not appear in " + filename)
-        sys.exit(1)
-    elif (wordTwo not in doc):
-        sys.stderr.write("Error: " + wordTwo + " does not appear in " + filename)
-        sys.exit(1)
-
     sorted_doc = (sorted(doc.items(), key = operator.itemgetter(1)))[::-1]
     just_the_occur = []
     just_the_rank = []
-    wordOne_rank, wordTwo_rank = 0, 0
+    word_rank = 0
     word_frequency = 0
+    entry_word = 1
 
-    entry_numOne, entry_numTwo = 1, 1
+    for i, word in enumerate(wordArg):
+        if (wordArg[i] not in doc):
+            sys.stderr.write("Error: " + wordArg[i] + " does not appear in " + filename)
+            sys.exit(1)
+        
+        for entry in sorted_doc:
+            if(entry[0] == wordArg[i]):
+                word_rank = entry_word
+                word_frequency = entry[1]
+                
+            just_the_rank.append(entry_word)
+            entry_word += 1
+            just_the_occur.append(entry[1])
 
-    for entry in sorted_doc:
-        if(entry[0] == wordOne):
-            wordOne_rank = entry_numOne
-            wordOne_frequency = entry[1]
-        elif (entry[0] == wordTwo):
-            wordTwo_rank = entry_numTwo
-            wordTwo_frequency = entry[1]
-
-        just_the_rank.append(entry_numOne)
-        entry_numOne += 1
-        entry_numTwo += 1
-        just_the_occur.append(entry[1])
-    
-    plt.title("Word Frequencies in " + filename)
-    plt.ylabel("Total Number of Occurrences")
-    plt.xlabel("Rank of word(\"" + wordOne + "\" is rank " + str(wordOne_rank) + ")" 
-    + "\n" + "Rank of word(\"" + wordTwo + "\" is rank " + str(wordTwo_rank) + ")")
-    
-    plt.loglog(just_the_rank, just_the_occur, basex=10)
-    # First word
-    plt.scatter(
-        [wordOne_rank],
-        [wordOne_frequency],
-        color="r",
-        marker=r"$ {} $".format(wordOne),
-        s=1000,
-        label=wordOne
+        plt.scatter(
+            [word_rank],
+            [word_frequency],
+            color="r",
+            marker=r"$ {} $".format(wordArg[i]),
+            s=1000,
+            label=wordArg[i]
         )
-    # Second word
-    plt.scatter(
-        [wordTwo_rank],
-        [wordTwo_frequency],
-        color="g",
-        marker=r"$ {} $".format(wordTwo),
-        s=1000,
-        label=wordTwo
-    )
+        plt.loglog(just_the_rank, just_the_occur, basex=10)    
+
+    plt.title("Word Frequencies in " + filename)
+    plt.ylabel("Total Number of Occurrences")    
     plt.grid(True)
     plt.show()
   
